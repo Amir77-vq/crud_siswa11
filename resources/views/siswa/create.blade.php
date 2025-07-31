3d9efbf6a99660df0717d02f9a35906f4896c642
amiir77-vq <amyrxzzz@gmail.com>
2025-07-31 13:05:14 +0700

    Menambahkan index dan create di view siswa

---
+++ resources/views/siswa/create.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>ADD STUDENT PAGE</h1>
+  <H2>STUDENT ADDITION FORM</H2>
+  <a href="/">BACK</a>
+  <form>
+    <div>
+      <label for="">CLASS</label>
+      <select name="">
+        <option value="">XI PPLG 1</option>
+        <option value="">XI PPLG 2</option>
+        <option value="">XI PPLG 3</option>
+      </select>
+    </div>
+    <div>
+      <label for="">NAME</label> <br>
+      <input type="text" name="name">
+    </div>
+    <div>
+      <label for="">NISN</label> <br>
+      <input type="text" name="nisn">
+    </div>
+    <div>
+      <label for="">ADDRESS</label> <br>
+      <input type="text" name="address">
+    </div>
+    <div>
+      <label for="">EMAIL</label> <br>
+      <input type="text" name="email">
+    </div>
+    <div>
+      <label for="">PASSWORD</label> <br>
+      <input type="password" name="password">
+      <div>
+      <label for="">NO. HANDPHONE</label> <br>
+      <input type="text" name="no_handphone">
+    </div>
+      <div>
+      <label for="">PHOTO</label> <br>
+      <input type="file" name="photo">
+    </div>
+    <button type="submit">SAVE</button>
+    <button type="reset">RESET</button>
+  </form>
+</body>
+</html>
